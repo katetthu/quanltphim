a756a608820773c16d50195e414ed8e084824d65
Nguyen Quynh Gia Thu <72873768+katetthu@users.noreply.github.com>
2021-06-14 20:43:44 +0700

    create

---
+++ QuanLyMovies/QuanLyMovies/ViewModels/UtilViewModel.cs
@@ -0,0 +1,29 @@
+﻿using QuanLyMovies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLyMovies.ViewModels
+{
+    public class UtilViewModel : DependencyObject
+    {
+        public static readonly DependencyProperty DSMovieProperty;
+
+        static UtilViewModel()
+        {
+            DSMovieProperty = DependencyProperty.Register("DSMovie", typeof(IList<Movie>), typeof(UtilViewModel));
+        }
+        public IList<Movie> DSMovie
+        {
+            get => (IList<Movie>)GetValue(DSMovieProperty);
+            set => SetValue(DSMovieProperty, value);
+        }
+        public UtilViewModel()
+        {
+            DSMovie = Enumerable.Range(0, 100).Select(i => new Movie()).ToList();
+        }
+    }
+}
---
+++ QuanLyMovies/QuanLyMovies/Views/TrangChuView.xaml.cs
@@ -0,0 +1,57 @@
+﻿using QuanLyMovies.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLyMovies.Views
+{
+    /// <summary>
+    /// Interaction logic for TrangChuView.xaml
+    /// </summary>
+    public partial class TrangChuView : Window
+    {
+        UtilViewModel vm;
+        public TrangChuView()
+        {
+            InitializeComponent();
+            // Manually alter window height and width
+            this.SizeToContent = SizeToContent.Manual;
+
+            //// Automatically resize width relative to content
+            //this.SizeToContent = SizeToContent.Width;
+
+            //// Automatically resize height relative to content
+            //this.SizeToContent = SizeToContent.Height;
+
+            //// Automatically resize height and width relative to content
+            //this.SizeToContent = SizeToContent.WidthAndHeight;.
+
+            vm = new UtilViewModel();
+            DataContext = vm;
+        }
+
+        private void Grid_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var grid = sender as Grid;
+            var me = grid.FindName("video") as MediaElement;
+            me?.Play();
+        }
+
+        private void Grid_MouseLeave(object sender, MouseEventArgs e)
+        {
+            var grid = sender as Grid;
+            var me = grid.FindName("video") as MediaElement;
+            me?.Stop();
+        }
+    }
+}
